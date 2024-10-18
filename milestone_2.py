9fde1c6e1e0b96887f7b163f4694e8a5bd3daa0e
Eleanor Williams <eleanorboman@hotmail.com>
2024-10-18 15:46:31 +0100

    Update milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -2,10 +2,8 @@ import random
 
 word_list = ["apple", "banana", "kiwi", "strawberry", "peach"]
 print (word_list)
-print(random.choice(word_list))
 
 word = random.choice(word_list)
-
 print (word)
 
 print ('Enter a single letter: ')
@@ -13,8 +11,10 @@ guess = input()
 
 while True:
     guess = input("Enter a single letter: ")
-    if len(guess) == 1 and isalpha(guess):
+    if len(guess) == 1:
+        print(f'Good guess!')
+        break
+    if guess.isalpha == True:
         print(f'Good guess!')
         break
     print("Oops! That is not a valid input.\n")
-
